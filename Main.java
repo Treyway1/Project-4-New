5f19a655e5ec1fb4c03356f87308bc883aa46ad1
Treyway1 <treyvor.romero-jones@patelhs.org>
2021-02-04 12:57:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) 
+   {  
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("What is your name?");
+    String name = scan.next();
+    System.out.println("?");
+    int ________ = scan.next();
+    System.out.println();
+    int ______ = scan.next();
+    
+  }
+}
